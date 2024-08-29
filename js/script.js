9231721e162cb46ac649535e189371a0a5699ed3
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-08-29 15:49:21 +0200

    ho creato la nuova lista di invitati vip

--- js/script.js
+++ js/script.js
@@ -21,10 +21,15 @@ window.addEventListener("load", (event) => {
     console.log("page is fully loaded");
   });
   // creo il mio array di invitati
-  const vipGuests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez',
+const vipGuests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez',
   'Chiara Ferragni','George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
- console.log(vipGuests);
+const newVipList = [];
+//  voglio aggiungere nuove proprieta per ogni elemento array
 
+ vipGuests.forEach((vipGuest, i, arr)=>{
+    newVipList.push({tableName: 'TavoloVip', guestName: vipGuest, place: (i+1)})
+ })
+console.log(newVipList);
 
 
 
