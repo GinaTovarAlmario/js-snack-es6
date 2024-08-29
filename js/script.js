ee15aac4d0ec778b17c2ab8cde03a735bc32b960
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-08-29 15:07:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,78 @@
+/**
+ * *SNACK 1*
+Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
+Ci ha lasciato il nome del tavolo ("Tavolo Vip") e la lista degli invitati in ordine di posto:
+```
+[ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',
+ 'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
+```
+Ma la tipografia per stampare il tutto vuole che le mandiamo una lista di ospiti in un formato specifico,
+per cui dobbiamo fare in modo che ogni ospite sia un oggetto javascript che ha come proprietà:
+nome del tavolo (tableName),
+nome dell'ospite (guestName),
+posto occupato (place),
+Generiamo e stampiamo in console la lista per i segnaposto.
+ */
+
+// controllo che veda il mio script.js
+console.log('Js ok');
+// ho fatto un file a parte per le mie funzioni faccio il controllo
+window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+  });
+  // creo il mio array di invitati
+  const vipGuests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez',
+  'Chiara Ferragni','George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
+ console.log(vipGuests);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /**
+ * *SNACK 2*
+Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista
+contenente il loro nome tutto in maiuscolo
+ES (Marco della Rovere => MARCO DELLA ROVERE);
+2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+Questo è l'elenco degli studenti:
+Id  Name                Grades
+213 Marco della Rovere      78
+110 Paola Cortellessa       96
+250 Andrea Mantegna 	    48
+145 Gaia Borromini          74
+196 Luigi Grimaldello 	    68
+102 Piero della Francesca   50
+120 Francesca da Polenta    84
+ */
