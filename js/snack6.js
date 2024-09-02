2c99b777151b82bfcb0e80f4830dd72c4fe83f3a
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-09-02 12:32:57 +0200

    snack 7

--- js/snack6.js
+++ js/snack6.js
@@ -7,12 +7,7 @@
 //   { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
 // ]
 // Crea un nuovo array con la lista dei mammiferi.
-
-
-
-
-
-const animals = [
+/*const animals = [
     { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
     { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
     { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
@@ -27,4 +22,43 @@ const animals = [
 console.log(animals);
 
 const mammals = animals.filter(element => element.classe === 'mammiferi');
-console.log(mammals);
+console.log(mammals); */
+
+// SNACK NUMERO 7
+// Crea un array di oggetti che rappresentano delle persone.
+// Ogni persona ha un nome, un cognome e un'età.
+
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e
+// l'indicazione se può guidare, in base all'età.
+
+
+const persons = [
+    {nome:'maria', cognome:'rossi',età:'33'},
+    {nome:'luigi', cognome:'pippo',età:'13'},
+    {nome:'marco', cognome:'plutone',età:'25'},
+    {nome:'sveva', cognome:'abate',età:'18'},
+    {nome:'chiara', cognome:'dufour',età:'17'},
+    {nome:'dasha', cognome:'sole',età:'67'},
+    {nome:'mirko', cognome:'verdi',età:'78'},
+    {nome:'laura', cognome:'kogut',età:'66'},
+    {nome:'paolo', cognome:'bruzzo',età:'9'},
+    {nome:'dario', cognome:'dolores',età:'10'},
+    {nome:'luca', cognome:'carvajal',età:'17'},
+    {nome:'irene', cognome:'rosetta',età:'30'},
+    {nome:'alberto', cognome:'paperino',età:'19'},
+];
+console.log(persons);
+
+// devo creare un nuovo array
+const newArray = persons.map((element) =>{
+    // for(let persona of persons){
+        const { nome, cognome, età} = element;
+        if(età >= 18){
+            element = `${nome} ${cognome} : ha ${età} e può guidare`;
+        } else {
+            element = `${nome} ${cognome} : ha ${età} e non può guidare`;
+        }
+    // }
+    return (element);
+});
+console.log(newArray);
